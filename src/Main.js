2c30028df94544307c9e68696d9b6d7db6289692
Joyce <joycemanning@gmail.com>
2021-01-04 12:42:35 -0500

    timeline add

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './main.css';
+import { Timeline } from 'antd';
+
+
 
 class Main extends React.Component {
 
+
   render() {
+    
     return (
       <div>
-        <img src='https://gameznet.com.au/animals/themes/fox/animated/giphy-1.gif'/>
+          <Timeline>
+            <Timeline.Item color="green">Abt</Timeline.Item>
+            <Timeline.Item color="red">Cnt</Timeline.Item>
+            <Timeline.Item color="blue">Lnx</Timeline.Item>
+            <Timeline.Item color="gray">Mc</Timeline.Item>
+          </Timeline>
+          <img src='https://gameznet.com.au/animals/themes/fox/animated/giphy-1.gif'/>
       </div>
     )
   }
