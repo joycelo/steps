87e5ec9558f95e316ad13eaf6ced52761af53fbc
Joyce <joycemanning@gmail.com>
2021-01-04 11:17:08 -0500

    fox add

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ class Main extends React.Component {
   render() {
     return (
       <div>
-        <img src='https://cdn.lowgif.com/full/05169b42d666cd0a-.gif' width='50%' height='50%'/>
+        <img src='https://gameznet.com.au/animals/themes/fox/animated/giphy-1.gif'/>
       </div>
     )
   }
