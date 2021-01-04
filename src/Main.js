bb0d48bb8f4475450dffe26ef5b05842c5b977f8
Joyce <joycemanning@gmail.com>
2021-01-04 12:46:36 -0500

    popover add

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './main.css';
-import { Timeline } from 'antd';
+import { Timeline, Popover } from 'antd';
 
 
 
@@ -8,6 +8,12 @@ class Main extends React.Component {
 
 
   render() {
+
+    const popoverContent = (
+      <div>
+        <p>po ta to</p>
+      </div>
+    );
     
     return (
       <div>
@@ -17,7 +23,9 @@ class Main extends React.Component {
             <Timeline.Item color="blue">Lnx</Timeline.Item>
             <Timeline.Item color="gray">Mc</Timeline.Item>
           </Timeline>
-          <img src='https://gameznet.com.au/animals/themes/fox/animated/giphy-1.gif'/>
+          <Popover content={popoverContent} title="tdy">
+            <img src='https://gameznet.com.au/animals/themes/fox/animated/giphy-1.gif'/>
+          </Popover>
       </div>
     )
   }
