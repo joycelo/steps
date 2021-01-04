10e9749e90e19c7fe8671abfdfe8da1586bc3bc1
Joyce <joycemanning@gmail.com>
2021-01-04 11:10:08 -0500

    first

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './main.css';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <img src='https://cdn.lowgif.com/full/05169b42d666cd0a-.gif' width='50%' height='50%'/>
+      </div>
+    )
+  }
+
+}
+
+export default Main;
